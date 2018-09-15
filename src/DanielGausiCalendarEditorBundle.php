4d6aae9a4838ac49e38524242c179f7c137fd752
Daniel <mail@gausi.de>
2018-09-15 20:32:18 +0200

    bundle??

---
+++ src/DanielGausiCalendarEditorBundle.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @copyright  Daniel Gaußmann 2018
+ * @author     Daniel Gaußmann (Gausi)
+ * @package    Calendar_Editor
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/DanielGausi/Contao-CalendarEditor
+ *
+ */
+
+namespace DanielGausi\CalendarEditor;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+
+class DanielGausiCalendarEditorBundle extends Bundle
+{
+}
