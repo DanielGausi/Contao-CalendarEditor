c24a59ff8102496ce20d4c88662045885ab673b4
Daniel <mail@gausi.de>
2018-09-18 20:55:43 +0200

    language, palette

--- src/Resources/contao/languages/de/tl_calendar.php
+++ src/Resources/contao/languages/de/tl_calendar.php
@@ -15,20 +15,13 @@
  * permanent text corrections which are update-safe. 
  */
  
-$GLOBALS['TL_LANG']['tl_calendar']['AllowEdit']['0'] = "Bearbeitung im Frontend erlauben";
-$GLOBALS['TL_LANG']['tl_calendar']['AllowEdit']['1'] = "Erlaubt Frontend-Usern das Hinzufügen und Bearbeiten von Terminen in diesem Kalender.";
-$GLOBALS['TL_LANG']['tl_calendar']['caledit_onlyFuture']['0'] = "Nur zukünftige Events";
-$GLOBALS['TL_LANG']['tl_calendar']['caledit_onlyFuture']['1'] = "Wenn aktiviert, ist die Bearbeitung nur für zukünftige Termine möglich.";
-$GLOBALS['TL_LANG']['tl_calendar']['caledit_jumpTo']['0'] = "Weiterleitungsseite zum Bearbeiten";
-$GLOBALS['TL_LANG']['tl_calendar']['caledit_jumpTo']['1'] = "Bitte wählen Sie eine Seite mit dem Event-Editor, auf den der User bei einem Klick auf einen Bearbeiten-Link weitergeleitet wird.";
-$GLOBALS['TL_LANG']['tl_calendar']['caledit_loginRequired']['0'] = "Login benötigt für die bearbeitung (empfohlen)";
-$GLOBALS['TL_LANG']['tl_calendar']['caledit_loginRequired']['1'] = "Wenn aktiviert, können nur registrierte Nutzer (aus den gewählten Gruppen) Events bearbeiten/hinzufügen. Warnung: Sonst kann das JEDER Besucher der Seite.";
-$GLOBALS['TL_LANG']['tl_calendar']['caledit_onlyUser']['0'] = "Bearbeiten nur für den Besitzer erlauben";
-$GLOBALS['TL_LANG']['tl_calendar']['caledit_onlyUser']['1'] = "Wenn dies aktiviert ist, darf nur der Ersteller eines Termins diesen später bearbeiten.";
-$GLOBALS['TL_LANG']['tl_calendar']['caledit_groups']['0'] = "Erlaubte Mitgliedergruppen zum Bearbeiten";
-$GLOBALS['TL_LANG']['tl_calendar']['caledit_groups']['1'] = "Wählen Sie die Gruppen aus, die Termine hinzufügen und bearbeiten dürfen.";
-$GLOBALS['TL_LANG']['tl_calendar']['caledit_adminGroup']['0'] = "Frontend-Administratoren zum Bearbeiten";
-$GLOBALS['TL_LANG']['tl_calendar']['caledit_adminGroup']['1'] = "Frontend-Administratoren dürfen alle Termine bearbeiten, auch wenn \"nur für den Besitzer\" aktiviert ist.";
-$GLOBALS['TL_LANG']['tl_calendar']['edit_legend'] = "Bearbeitung im Frontend";
+$GLOBALS['TL_LANG']['tl_calendar']['allowEdit']             = array('Bearbeitung im Frontend erlauben', 'Erlaubt Frontend-Usern das Hinzufügen und Bearbeiten von Terminen in diesem Kalender.');
+$GLOBALS['TL_LANG']['tl_calendar']['caledit_onlyFuture']    = array('Nur zukünftige Events', 'Wenn aktiviert, ist die Bearbeitung nur für zukünftige Termine möglich.');
+$GLOBALS['TL_LANG']['tl_calendar']['caledit_jumpTo']        = array('Weiterleitungsseite zum Bearbeiten','Bitte wählen Sie eine Seite mit dem Event-Editor, auf den der User bei einem Klick auf einen Bearbeiten-Link weitergeleitet wird.)';
+$GLOBALS['TL_LANG']['tl_calendar']['caledit_loginRequired'] = array('Login benötigt für die bearbeitung (empfohlen)', 'Wenn aktiviert, können nur registrierte Nutzer (aus den gewählten Gruppen) Events bearbeiten/hinzufügen. Warnung: Sonst kann das JEDER Besucher der Seite.');
+$GLOBALS['TL_LANG']['tl_calendar']['caledit_onlyUser']      = array('Bearbeiten nur für den Besitzer erlauben','Wenn dies aktiviert ist, darf nur der Ersteller eines Termins diesen später bearbeiten.');
+$GLOBALS['TL_LANG']['tl_calendar']['caledit_groups']        = array('Erlaubte Mitgliedergruppen zum Bearbeiten','Wählen Sie die Gruppen aus, die Termine hinzufügen und bearbeiten dürfen.');
+$GLOBALS['TL_LANG']['tl_calendar']['caledit_adminGroup']    = array('Frontend-Administratoren zum Bearbeiten','Frontend-Administratoren dürfen alle Termine bearbeiten, auch wenn \'nur für den Besitzer\' aktiviert ist.');
+$GLOBALS['TL_LANG']['tl_calendar']['edit_legend'] = 'Bearbeitung im Frontend';
 
 ?>
