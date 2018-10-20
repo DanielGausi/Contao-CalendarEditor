00e033e7de8dab0337e407f8a6a53f77ae56ce89
Daniel <mail@gausi.de>
2018-10-20 13:47:54 +0200

    wuppdi

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -25,11 +25,12 @@ class Plugin implements BundlePluginInterface
     public function getBundles(ParserInterface $parser)
     {
         return [
-            BundleConfig::create(CalendarEditorBundle::class)
+            BundleConfig::create('DanielGausi\CalendarEditorBundle\DanielGausiCalendarEditorBundle')
                 ->setLoadAfter(
 					[
 						'Contao\CoreBundle\ContaoCoreBundle',
-						'Contao\CalendarBundle\ContaoCalendarBundle'
+						'Contao\CalendarBundle\ContaoCalendarBundle',
+						'MenAtWork\MultiColumnWizard'
 					]
 				)
         ];
